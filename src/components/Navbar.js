3fe88e260734f3fe66d757d9943d5eedc6fe3315
Haven Rutter <havenbeats1@gmail.com>
2022-11-07 12:04:44 -0500

    hotels

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import logo from '../images/logo.svg'
+import {FaAlignRight} from 'react-icons/fa'
+import {Link} from 'react-router-dom'
+
+
+export default class Navbar extends Component {
+    state={
+        isOpen:false
+    }
+    handleToggle = () =>{
+        this.setState({isOpen:!this.state.isOpen})
+    }
+  render() {
+    return (
+      <div>Navbar</div>
+    )
+  }
+}
+12
